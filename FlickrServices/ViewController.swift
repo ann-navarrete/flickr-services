25a4b9cd5def30e62b1454bd362b5536c7cce411
Ann Navarrete <ann.navarrete@xero.com>
2020-12-14 13:35:30 +1100

    Initial Commit

---
+++ FlickrServices/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlickrServices
+//
+//  Created by Ann Navarrete on 14/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
